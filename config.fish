efebe18ea0d8e19de1d397b99630f52575fa429e
Joshua Peisach <itzswirlz2020@outlook.com>
2022-12-08 11:29:56 -0500

    Create config.fish

---
+++ config.fish
@@ -0,0 +1,13 @@
+# Just add this:
+alias ls="exa"
+alias cat="bat"
+alias find="fd"
+alias sed="sd"
+alias du="dust"
+alias grep="rg"
+alias man="tldr"
+alias diff="difft"
+alias tree="broot"
+
+atuin init fish | source
+zoxide init fish | source
