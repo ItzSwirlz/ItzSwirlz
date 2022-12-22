ed12dc150abbba70f7d4190170c48cb42fe91152
Joshua Peisach <itzswirlz2020@outlook.com>
2022-12-22 14:17:48 -0500

    update fish config

--- config.fish
+++ config.fish
@@ -1,4 +1,6 @@
-# Just add this:
+set PATH $PATH:/opt/homebrew/bin:/opt/local/bin:$HOME/.cargo/bin:/opt/homebrew/opt/openjdk/bin:/opt/flutter/bin:/opt/checker-279/bin
+set GPG_TTY $(tty)
+
 alias ls="exa"
 alias cat="bat"
 alias find="fd"
@@ -8,6 +10,15 @@ alias grep="rg"
 alias man="tldr"
 alias diff="difft"
 alias tree="broot"
+#alias git="gix"  still needs work
+
+alias commit="git commit -S"
 
 atuin init fish | source
 zoxide init fish | source
+
+eval (zellij setup --generate-auto-start fish | string collect)
+
+if status is-interactive
+    eval (zellij setup --generate-auto-start fish | string collect)
+end
